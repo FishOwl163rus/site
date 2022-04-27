b2c1b18150a093bcce42d71c347e5caf66bd34d2
Maksim Ramanau <MRamanau@ibagroup.eu>
2022-04-27 04:04:39 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu__list");
+const navLink = document.querySelectorAll(".menu__link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
